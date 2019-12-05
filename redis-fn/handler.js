ac2846a7a12e23af98427ba332ec23d37f1cbac9
LLLLinda <lindacheung512@gmail.com>
2019-12-05 12:29:20 +0800

    fix bug

--- redis-fn/handler.js
+++ redis-fn/handler.js
@@ -5,13 +5,13 @@ const redis = require("redis");
 module.exports = (body, callback) => {
 
     const currentStat = JSON.parse(body).current_stat;    
-    const name = JSON.parse(body).name;
+    const name = JSON.parse(body).playername;
 
 
     const client = redis.createClient(6379, process.env.redis);
 
     client.set(name, currentStat, () => {
       client.quit();
-      callback(null, { status: 'set done' });
+      callback(null, { 'status': 'set done', "name":name,"saved status":currentStat });
     });
 }
